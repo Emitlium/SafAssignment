69272155f50ad5fc2d5cf16acd656e36fd7bdd08
Michaela <88110578+Emitlium@users.noreply.github.com>
2023-10-13 22:55:02 +0200

    Add project files.

---
+++ SafAssignment/LogovaciTrida.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafAssignment
+{
+    public class LogovaciTrida
+    {
+        /* Tady bude:
+         * Vytvořte logovací třídu, která bude zapisovat (např. do souboru) jednotlivé události a  akce.
+         * Výpis bude obsahovat aktuální čas a zprávu.
+         * Přidávejte logy z běhu testů podle  uvážení, aby mohly sloužit pro případné ladění při selhání testu. 
+         */
+
+    }
+}
